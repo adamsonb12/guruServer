186cfecf4cfd6f91ed76985a8b3b31baf0d07450
Brett Adamson <badamson@contractor.youniqueproducts.com>
2018-09-08 11:40:28 -0600

    removed example route and query

--- guru.js
+++ guru.js
@@ -11,15 +11,6 @@ module.exports = () => {
     res.send({ go: 'the password is guest' });
   });
 
-  // this is an example route here to show how to use the models to query for an record
-  // See the bookshelf documentation for more examples and documentation
-  guru.get('/user', (req, res) => {
-    new User({ email: 'slayerofyounglings@test.com' }).fetch().then(function(user) {
-      console.log('user', user);
-    });
-    res.send({ hello: 'there ' });
-  });
-
   routes(guru);
 
   guru.listen(5000);
