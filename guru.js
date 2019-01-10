568907148b7582c95274871e8d58785d18d68e23
Brett Adamson <badamson@contractor.youniqueproducts.com>
2019-01-09 20:17:00 -0700

    add in user auth

--- guru.js
+++ guru.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-
+const passport = require('passport');
+const session = require('express-session');
 const routes = require('./controllers/routes');
+require('./services/passport');
 
 module.exports = () => {
     const guru = express();
 
     guru.use(express.json());
+
+    guru.use(session({ secret: 'cats' }));
+    guru.use(passport.initialize());
+    guru.use(passport.session());
 
     guru.get('/', (req, res) => {
         res.send({ go: 'the password is guest' });
