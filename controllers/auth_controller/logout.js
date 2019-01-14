2176cd6935c7b0488ead7fb2ae7b9cacb2d5e42c
Brett Adamson <badamson@contractor.youniqueproducts.com>
2019-01-14 14:55:31 -0700

    refactored bad code

--- controllers/auth_controller/logout.js
+++ controllers/auth_controller/logout.js
@@ -1,12 +1,11 @@
 module.exports = (req, res) => {
     req.logout();
     req.session.destroy(err => {
-        if (!err) {
-            res.status(200)
-                .clearCookie('connect.sid', { path: '/' })
-                .send({ status: 'Logged out' });
-        } else {
+        if (err) {
             throw new Error(err);
         }
+        res.status(200)
+            .clearCookie('connect.sid', { path: '/' })
+            .send({ status: 'Logged out' });
     });
 };
