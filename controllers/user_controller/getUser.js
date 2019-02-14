f256ceaf069063f0007a62c1b75995b993e160e7
Brett Adamson <badamson@contractor.youniqueproducts.com>
2019-02-13 18:16:31 -0700

    passwords not returned any more to front end, if we need it, we should create a specific endpoint for it, or add validation to the user endpoints to see if they have access to secure info

--- controllers/user_controller/getUser.js
+++ controllers/user_controller/getUser.js
@@ -2,7 +2,9 @@ const { User } = require('../../models');
 
 module.exports = async (req, res, next) => {
     try {
-        const user = await new User({ id: req.query.user_id }).fetch();
+        const user = await new User({ id: req.query.user_id }).fetch({
+            columns: ['id', 'name_first', 'name_last', 'name_middle', 'email', 'date_birth'],
+        });
         if (user) {
             res.status(200).send({ user: user });
         } else {
